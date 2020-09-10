54d344abe63143e5c02ac067914b1f737bbe5a8e
Raynald22 <44433953+Raynald22@users.noreply.github.com>
2020-09-10 14:34:53 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'email', 'form_validation', 'table', 'template', 'cart', 'pagination', 'table');
+$autoload['libraries'] = array('database', 'session', 'email', 'form_validation', 'table', 'cart', 'pagination', 'table');
 
 /*
 | -------------------------------------------------------------------
